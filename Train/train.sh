8c181c52f69bb920eaad0d7f035a6b17107a6536
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2024-04-21 08:36:25 +1000

    Update train.sh

--- Train/train.sh
+++ Train/train.sh
@@ -3,7 +3,7 @@ train_single_source_gem_coslr_wb_balance_cos_ema_ddpmm_pattern_condition_torch2.
 -ds anypattern_v31 -a vit_base_pattern_condition_first_dgg --margin 0.0 \
 --num-instances 4 -b 512 -j 64 --warmup-step 5 \
 --lr 0.00035 --iters 10000 --epochs 25 \
---data-dir /path/to/images/ \
+--data-dir /path/to/ \
 --logs-dir logs/anypattern_v31/vit_two_losses_m0.6_ddpmm_8gpu_512_10000_torch2_pattern_condition_first_dgg \
 --height 224 --width 224 \
 --multiprocessing-distributed --world-size 1
